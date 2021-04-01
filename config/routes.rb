06287fe62888bf385741713f49cb833a62a0bf86
GT301 <taoguo301@gmail.com>
2021-04-01 03:20:41 -0500

    Modified routes.rb and partials of nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'categories/index'
-  get 'categories/show'
-  get 'products/index'
-  get 'products/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :categories, only: %i[index show]
+  resources :products, only: %i[index show]
+
+  root to: "products#index"
 end
