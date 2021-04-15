f10982916cefa4819801d602871f3682fad29ee4
GT301 <taoguo301@gmail.com>
2021-04-15 04:07:17 -0500

    Added search function for the products by inputing keywords or selecting categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,21 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @products = if params[:search_term].blank?
+                  Product.all
+                elsif params[:c_select].blank?
+                  Product.where("product_name LIKE ? OR description LIKE ?",
+                                "%#{params[:search_term]}%",
+                                "%#{params[:search_term]}%")
+                else
+
+                  Product.where("product_name LIKE ? OR description LIKE ?",
+                                "%#{params[:search_term]}%",
+                                "%#{params[:search_term]}%")
+                         .where("category_id == ?", params[:c_select].to_s)
+
+                end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :categories, only: %i[index show]
-  resources :products, only: %i[index show]
+  resources :products, only: %i[index show] do
+    collection do
+      get :search
+    end
+  end
 
   root to: "products#index"
 end
