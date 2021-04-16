34dde372c7be592e9964b7d3fd7518001c32051a
GT301 <taoguo301@gmail.com>
2021-04-16 00:14:31 -0500

    Created cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
     session[:shopping_cart] ||= []
     # an array of product IDs!
   end
+
+  def cart
+    # .find will take in a single value OR an array of values.
+    # with an array it will bring back a collection of objects based on those PK id's!
+    Product.find(session[:shopping_cart])
+  end
 end
