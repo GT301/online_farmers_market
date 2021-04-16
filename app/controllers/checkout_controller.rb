03b430bcd589271451fa68655a4ed5193329b763
GT301 <taoguo301@gmail.com>
2021-04-16 03:23:14 -0500

    Create route for checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,13 @@
 class CheckoutController < ApplicationController
+  def create
+    # Establish a connection with Stripe and then redirect the user to the payment screen.
+  end
+
+  def success
+    # We took the customer's money!
+  end
+
+  def cancel
+    # Something went wrong with the payment :(
+  end
 end
